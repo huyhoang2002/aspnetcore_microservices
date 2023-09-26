e2eb9d8e3cea01a308bfe5996ecafdc9b6eaec6c
huy.hoang <huy.hoang@orientsoftware.com>
2023-09-26 10:38:38 +0700

    feat: catalog api

---
+++ AspnetcoreMicroservices/AspnetCore-Microservices/catalog-service/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(_ => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(getPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> getPreconfiguredProducts()
+        {
+            return new List<Product>
+            {
+                new Product("product 1", "category 1", "abc", "def", "url1", 100),
+                new Product("product 1", "category 1", "abc", "def", "url1", 100),
+                new Product("product 1", "category 1", "abc", "def", "url1", 100)
+            };
+        }
+    }
+}
---
+++ AspnetcoreMicroservices/AspnetCore-Microservices/catalog-service/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+        }
+
+        public async Task DeleteAll()
+        {
+            await _context.Products.DeleteManyAsync(Builders<Product>.Filter.Empty);
+        }
+
+        public Task<bool> DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context.Products.Find(_ => _.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            return await _context.Products.Find(product => product.Category == categoryName).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            return await _context.Products.Find(product => product.Name == name).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.Find(new BsonDocument()).ToListAsync();
+        }
+
+        public Task<bool> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
